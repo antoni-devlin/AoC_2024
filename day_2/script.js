59a7e1f8bb0c260159ffa1c3509fca9a0e86894c
Antoni <antoni-devlin@users.noreply.github.com>
2024-12-02 13:45:14 +0000

    Successfully converted input to nested array of ints

--- day_2/script.js
+++ day_2/script.js
@@ -1,5 +1,24 @@
 import { readFile } from "../utilities/readFile.js";
 
+// Read puzzle input
 const input = await readFile("input.txt");
 
-console.log(input);
+// Split input into individual reports
+let reports = input.split("\n");
+
+// Split codes into levels for parsing
+reports = reports.map((code) => code.split(" "));
+
+// Convert levels from string to int
+reports = reports.map((report) => report.map((level) => Number(level)));
+
+console.log(reports);
+
+// function isSorted(arr) {
+//   console.log(arr);
+//   return arr.reduce((isSorted, value, index) => {
+//     return isSorted && (index === 0 || arr[index - 1] <= value);
+//   }, true);
+// }
+
+// console.log(isSorted(codes[4]));
