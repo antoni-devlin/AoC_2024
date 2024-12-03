3e62b0a107e2284017225024ce4fd82df34c6f1d
Antoni <antoni-devlin@users.noreply.github.com>
2024-12-03 22:08:47 +0000

    wip

--- day_3/script.js
+++ day_3/script.js
@@ -3,12 +3,35 @@ import { readFile } from "../utilities/readFile.js";
 // Read puzzle input
 const input = await readFile("input.txt");
 
-const pattern = /mul\(\d{1,3},\d{1,3}\)/g;
+// const pattern = /mul\(\d{1,3},\d{1,3}\)/g;
 
-let matches = input
-  .match(pattern)
-  .map((match) => match.slice(4, -1))
-  .map((match) => match.split(","))
-  .map((match) => match.map((number) => Number(number)));
+const firstPattern = /^(.*?)don\'t\(\)/g;
 
-console.log(matches);
+const pattern =
+  /do\(\).*?(mul\(\d{1,3},\d{1,3}\)).*?(mul\(\d{1,3},\d{1,3}\))*.*don\'t\(\)/g;
+
+// do() and don't()
+// ^do\(\).*(mul\(\d{1,3},\d{1,3}\)+.*don\'t\(\)$
+
+let sumOfProducts = 0;
+
+let initialMatches = input.match(firstPattern);
+
+initialMatches = initialMatches[0].match(pattern);
+
+console.log(initialMatches);
+
+// let matches = input
+//   .match(pattern)
+//   .map((match) => match.match(/mul\(\d{1,3},\d{1,3}\)/g));
+
+// matches = matches
+//   .flat()
+//   .map((match) => match.slice(4, -1).split(","))
+//   .map((match) => match.map((number) => Number(number)));
+
+// matches.forEach((element) => {
+//   sumOfProducts += element[0] * element[1];
+// });
+
+// console.log(sumOfProducts);
