61bd64eac7985396c326fd44bde5c487c4a14dff
Antoni <antoni-devlin@users.noreply.github.com>
2024-12-04 11:44:53 +0000

    wip

---
+++ day_4/notes.md
@@ -0,0 +1,24 @@
+Find all instances of the string "XMAS" in a 2D array (i.e. matrix).
+
+1. Traverse the matrix left to right, top to bottom ([0,0][0,1][0,2][0,3])
+2. When you find an "X", check surrounding cells for an "M". The cells to be checked will be:
+
+[TL] [U] [TR]
+[L ] [X] [ R]
+[BL] [D] [BR]
+
+- left: [x,y-1]
+- right: [x, y+1]
+- up: [x-1, y]
+- down: [x+1, y]
+- top-left: [x-1, y-1]
+- top-right: [x-1, y+1]
+- bottom-left: [x+1,y-1]
+- bottom-right: [x+1,y+1]
+
+```
+function gridSearch(coodinates, letter) {
+
+}
+
+```
--- day_4/script.js
+++ day_4/script.js
@@ -5,13 +5,20 @@ const input = await readFile("input.txt");
 
 const matrix = input.split("\n").map((line) => line.split(""));
 
+const search = ["XMAS", "SAMX"];
+
 function traverseMatrix(matrix) {
   const dimensions = [matrix.length, matrix[0].length];
 
-  matrix.forEach((line) => {
-    line.forEach((letter) => {
+  matrix.forEach((line, x) => {
+    line.forEach((letter, y) => {
+      let coordinates = [x, y];
+      console.log(coordinates[0], coordinates[1]);
       if (letter === "X") {
-        console.log("It's an X!");
+        console.log(`X found at ${coordinates}`);
+        if (matrix[coordinates[0 + 1][1]] === "M") {
+          console.log(`Found M `);
+        }
       }
     });
   });
